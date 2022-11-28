00d57ae28db7a8f261a24d52cc0f3019efb36cb0
straight744 <119273273+straight744@users.noreply.github.com>
2022-11-28 09:57:38 +0700

    Add files via upload

---
+++ Belajar/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blog Kelompok</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>Blog Kelompok PKWU</b></h1>
+  <p>Selamat Datang Di Blog <span class="w3-tag">PKWU</span></p>
+</header>
+
+<!-- Grid -->
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col l8 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <img src="Danu.jpeg" alt="Nature" style="width:50%">
+    <div class="w3-container">
+      <h3><b>Anak No life x-4</b></h3>
+      <span class="w3-opacity">Lahir, 23 agustus 2006</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Fahmi Husaini adalah seorang ketua kelas x-4 tetapi dia adalah anak kesayangan dari romo/ishak botak</p>
+      <p></p>
+    </div>
+  </div>
+  <hr>
+
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="Chairul.jpeg" Norway style="width:100%; height: 30%;">
+    <div class="w3-container">
+      <h3><b>Chairul</b></h3>
+      <span class="w3-opacity">Lahir, 22 juli 2006</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Chairul adalah anak yang sangat malas di kelompok ini meskipun begitu dia adalah tipe orang pekerja karena memiliki badan yang bagus untuk menjadi kuli bangunan</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="Fahmi.jpeg" alt="Norway" style="width:50%">
+    <div class="w3-container">
+      <h3><b>Bandar Terkenal</b></h3>
+      <span class="w3-opacity">Lahir, 16 april 2007</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Zahrandanu telah ditangkap sejak tahun 2018 dan dikenakan hukuman penjara 100 tahun karena telah melanggar pasal 115 ayat 2 bagian c dan terancam hukuman mati</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="Alya.jpeg" alt="Norway" style="width:100%">
+    <div class="w3-container">
+      <h3><b>Alya</b></h3>
+      <span class="w3-opacity">Lahir, 22 agustus 2006</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Alya adalah istri dari Zahrandanu yang terkenal bandar dan cabul</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="Moira.jpeg" alt="Norway" style="width:100%">
+    <div class="w3-container">
+      <h3><b>Moira</b></h3>
+      <span class="w3-opacity">Lahir, 22 mei 2007</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Moira adalah</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="Tiyas.jpeg" alt="Norway" style="width:50%">
+    <div class="w3-container">
+      <h3><b>Tiyas</b></h3>
+      <span class="w3-opacity">Lahir, 8 desember 2006</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Tiyas adalah seorang anak terlantar yang ditemukan oleh romo dan dimasukkan ke sekolah Hang Tuah 1 oleh kedua orang tuanya</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="Danu 2.jpeg" alt="Norway" style="width:500px; height:500px">
+    <div class="w3-container">
+      <h3><b>Vanesya</b></h3>
+      <span class="w3-opacity">Lahir, 26 mei 2007</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Vanesya adalah anak dari kedua orang tuanya yang dilahirkan di atas kasur di rumah sakit</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="Amanda.jpeg" alt="Norway" style="width:100%">
+    <div class="w3-container">
+      <h3><b></b></h3>
+      <span class="w3-opacity">Lahir, 5 maret 2007</span>
+    </div>
+
+    <div class="w3-container">
+      <p>Amanda adalah seorang anak yang ambisius, tetapi dia terlalu suka bermain dengan temannya</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+      </div>
+    </div>
+  </div>
+  
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- Introduction menu -->
+<div class="w3-col l4">
+  <!-- About Card -->
+  <div class="w3-card w3-margin w3-margin-top">
+  <img src="Foto kelompok.jpeg" style="width:100%">
+    <div class="w3-container w3-white">
+      <h4><b>Tentang Kami</b></h4>
+      <p>Kami adalah kelompok yang terbentuk dari beberapa bulan yang lalu, tetapi kami sudah sangat kompak satu sama lain, sehingga dapat menyelsaikan pekerjaan dengan mudah</p>
+      <p>Ini adalah kelompok favorit kami diantara kelompok yang ada di matapelajaran yang lain</p>
+    </div>
+  </div><hr>
+  
+  <!-- Posts -->
+  
+ 
+  <!-- Labels / tags -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Tags</h4>
+    </div>
+    <div class="w3-container w3-white">
+    <p>
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Fauzan</button> 
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Moira</button>
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Zahrandanu</button> 
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Alya</button>
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Fahmi</button>
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Gendis</button> 
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Chairul</button> 
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Hasya</button>
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Tiyas</button> 
+       <button class="w3-tag w3-light-grey w3-small w3-margin-bottom">Vanesya</button> 
+    </p>
+    </div>
+  </div>
+  
+<!-- END Introduction Menu -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+  <p>@Copyright 2022</p>
+</footer>
+
+</body>
+</html>
